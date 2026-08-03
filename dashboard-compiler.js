8f9f2b3905a93f1cd7703cb0449b1ec749d587ea
Gregorio Bueno <greg@travelwithbears.com>
2026-08-02 20:16:38 -0500

    Add Cache-Control meta tags to index.html to force browser refresh and avoid loading cached stale deals

--- dashboard-compiler.js
+++ dashboard-compiler.js
@@ -223,6 +223,9 @@ function compileStaticDashboard() {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+  <meta http-equiv="Pragma" content="no-cache">
+  <meta http-equiv="Expires" content="0">
   <title>FAM Scout - Wandering Bear Travel Agency</title>
   
   <link rel="preconnect" href="https://fonts.googleapis.com">
